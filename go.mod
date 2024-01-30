dd7b83be903e43b600869e964e9916607dae2e7b
Marcos Lilljedahl <marcosnils@gmail.com>
2024-01-30 01:01:52 -0300

    test docker push
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- go.mod
+++ go.mod
@@ -1,21 +1,23 @@
 module github.com/marcosnils/test
 
-go 1.17
+go 1.21
+
+toolchain go1.21.0
 
 require (
-	dagger.io/dagger v0.7.2
+	dagger.io/dagger v0.9.7
 	github.com/estesp/manifest-tool/v2 v2.0.0-beta.1
 )
 
 require (
-	github.com/99designs/gqlgen v0.17.2 // indirect
+	github.com/99designs/gqlgen v0.17.43 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
-	github.com/Khan/genqlient v0.5.0 // indirect
+	github.com/Khan/genqlient v0.6.0 // indirect
 	github.com/adrg/xdg v0.4.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/containerd/containerd v1.5.5 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/deislabs/oras v0.8.1 // indirect
 	github.com/docker/cli v20.10.0-beta1+incompatible // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
@@ -27,12 +29,14 @@ require (
 	github.com/fatih/color v1.10.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.7.4-0.20190830121156-884b5ffcbd3a // indirect
 	github.com/iancoleman/strcase v0.2.0 // indirect
 	github.com/klauspost/compress v1.11.13 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/moby/locker v1.0.1 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
@@ -45,16 +49,18 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/sosodev/duration v1.2.0 // indirect
 	github.com/urfave/cli v1.22.2 // indirect
-	github.com/vektah/gqlparser/v2 v2.5.1 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sync v0.2.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.11 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
 	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
 	google.golang.org/grpc v1.33.2 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
--- main.go
+++ main.go
@@ -4,17 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
 	"dagger.io/dagger"
 )
-
-const dockerVer = "docker:24"
-
-type runtime struct {
-	ctx    context.Context
-	client *dagger.Client
-}
 
 func checkErr(err error) {
 	if err != nil {
@@ -31,117 +23,8 @@ func main() {
 	checkErr(err)
 	defer client.Close()
 
-	R := &runtime{
-		ctx:    ctx,
-		client: client,
-	}
+	id, err := client.Container().From("alpine").
+		Publish(ctx, "docker.io/marcosnils/test-alpine")
 
-	// docker-cli
-	base, err := R.baseContainer()
-	checkErr(err)
-
-	// docker daemon | dind
-	daemon, err := R.daemonContainer()
-	checkErr(err)
-
-	// attach deemon as a service to our base image
-	cntr, err := R.attachService(base, daemon)
-	checkErr(err)
-
-	go func() {
-		fmt.Println("starting daemon")
-		_, err = daemon.ExitCode(R.ctx)
-		checkErr(err)
-	}()
-
-	fmt.Println("sleeping")
-	time.Sleep(10 * time.Second)
-
-	err = R.dockerInfo(cntr)
-	checkErr(err)
-
-	err = R.dockerTest(cntr)
-	checkErr(err)
+	fmt.Println(id, err)
 }
-
-func (R *runtime) baseContainer() (*dagger.Container, error) {
-	c := R.client.Container().From(dockerVer + "-cli").
-		WithExec([]string{"apk", "add", "curl"})
-	c = c.Pipeline("base/image")
-
-	return c, nil
-}
-
-func (R *runtime) daemonContainer() (*dagger.Container, error) {
-	c := R.client.Container().From(dockerVer + "-dind")
-	c = c.Pipeline("daemon/image")
-
-	c = c.WithMountedCache("/tmp", R.client.CacheVolume("shared-tmp"))
-	c = c.WithExposedPort(2375)
-	c = c.WithMountedCache("/var/lib/docker", R.client.CacheVolume("shared-docker"))
-
-	c = c.WithExec(
-		[]string{
-			"dockerd",
-			"--log-level=info",
-			"--host=tcp://0.0.0.0:2375",
-			"--tls=false",
-		},
-		dagger.ContainerWithExecOpts{InsecureRootCapabilities: true},
-	)
-	return c, nil
-}
-
-func (R *runtime) attachService(c, s *dagger.Container) (*dagger.Container, error) {
-	t := c.Pipeline("docker/service")
-	t = t.WithEnvVariable("DOCKER_HOST", "tcp://global-dockerd:2375")
-	t = t.WithServiceBinding("global-dockerd", s)
-	t = t.WithMountedCache("/tmp", R.client.CacheVolume("shared-tmp"))
-
-	return t, nil
-}
-
-func (R *runtime) dockerInfo(c *dagger.Container) error {
-	t := c.Pipeline("docker/info")
-
-	t = t.WithExec([]string{"docker", "info"})
-
-	out, err := t.Stdout(R.ctx)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(out)
-	return nil
-}
-
-func (R *runtime) dockerTest(c *dagger.Container) error {
-	t := c.Pipeline("docker/test")
-
-	t = t.WithExec([]string{"docker", "pull", "nginxdemos/hello"})
-	t = t.WithExec([]string{"docker", "images"})
-	// t = t.WithExec([]string{"ls", "-l", "/sys/fs/cgroup"})
-	t = t.WithEnvVariable("CACHE", time.Now().String())
-	t = t.WithExec([]string{"docker", "run", "-p", "4000:80", "-d", "nginxdemos/hello"})
-	t = t.WithExec([]string{"curl", "global-dockerd:4000"})
-
-	_, err := t.Sync(R.ctx)
-
-	return err
-}
-
-func (R *runtime) daemonTest(c *dagger.Container) error {
-	t := c.Pipeline("daemon/test")
-
-	t = t.WithExec([]string{"ls", "-l", "/sys/fs"})
-
-	out, err := t.Stdout(R.ctx)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(out)
-
-	return err
-}
-
